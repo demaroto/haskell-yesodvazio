ab381f3afeaaa739653a2f2f8572ff543988ce58
Ubuntu <ubuntu@ip-172-31-89-198.ec2.internal>
2019-11-13 12:46:59 +0000

    Primeiro

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -6,8 +6,18 @@
 module Handler.Home where
 
 import Import
+import Text.Lucius -- css
+import Text.Julius
 import Network.HTTP.Types.Status
 import Database.Persist.Postgresql
 
 getHomeR :: Handler Html
-getHomeR = undefined
+getHomeR = do
+    defaultLayout $ do
+        addStylesheet (StaticR css_bootstrap_css)
+        toWidgetHead $(luciusFile "templates/aaa.lucius")
+        toWidgetHead $(luciusFile "templates/aaa.julius")
+        $(whamletFile "templates/aaa.hamlet")
+        
+getQualquerCoisaR :: Handler Html
+getQualquerCoisaR = undefined
