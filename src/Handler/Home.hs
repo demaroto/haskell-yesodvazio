a949dac4872d2b3d720b1b452a7da45abec72433
Zukkin <demaroto69@gmail.com>
2019-11-27 12:50:57 -0300

    Corrigindo_tabs

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -13,8 +13,8 @@ import Database.Persist.Postgresql
 getHomeR :: Handler Html
 getHomeR = do
     defaultLayout $ do
-        addStylesheet (StaticR css_CSS_css)
-        addStylesheet (StaticR css_Mobile_css)
+        addStylesheet (StaticR css_css_css)
+        addStylesheet (StaticR css_mobile_css)
         --toWidgetHead $(luciusFile "templates/aaa.lucius")
         toWidgetHead $(luciusFile "templates/home.julius")
         $(whamletFile "templates/home.hamlet")
