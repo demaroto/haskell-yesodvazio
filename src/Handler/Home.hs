fcc263b8de5c41f14a8abdd219045eddab219e70
Will <demaroto69@gmail.com>
2019-12-03 20:48:30 -0300

    bbbbbbbb

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeFamilies #-}
 module Handler.Home where
