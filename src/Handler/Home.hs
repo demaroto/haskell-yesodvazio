9e114fbdd31b529b900086514517b03007f1afc8
Will <demaroto69@gmail.com>
2019-12-03 22:09:12 -0300

    aaa

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -14,7 +14,7 @@ import Database.Persist.Postgresql
 getHomeR :: Handler Html
 getHomeR = do
     defaultLayout $ do
-        toWidgetHead([hamlet|<meta name=viewport content=#{width=device width, initial scale=1.0}>|])
+       -- toWidgetHead([hamlet|<meta name=viewport content=#{width=device width, initial scale=1.0}>|])
         addScriptRemote "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"
         toWidgetHead $(luciusFile "templates/css.lucius")
         toWidgetHead $(luciusFile "templates/mobile.lucius")
