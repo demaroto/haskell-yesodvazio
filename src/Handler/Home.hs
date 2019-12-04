5ff294239efaad07049102e515f7f5534b436712
Will <demaroto69@gmail.com>
2019-12-03 21:50:24 -0300

    aaa

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -8,11 +8,14 @@ module Handler.Home where
 
 import Import
 import Text.Lucius
+import Text.Julius
 import Database.Persist.Postgresql
 
 getHomeR :: Handler Html
 getHomeR = do
     defaultLayout $ do
+        addScriptRemote "https://code.jquery.com/jquery-3.4.1.js"
         toWidgetHead $(luciusFile "templates/css.lucius")
         toWidgetHead $(luciusFile "templates/mobile.lucius")
+        toWidgetHead $(luciusFile "templates/js.julius")
         $(whamletFile "templates/home.hamlet")
