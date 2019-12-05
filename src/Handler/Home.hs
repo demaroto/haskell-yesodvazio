2bc65a74ed11e970063395b2aad804b158ac49a2
Will <demaroto69@gmail.com>
2019-12-05 15:44:19 -0300

    Configurando banco

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -19,14 +19,13 @@ formSig = renderBootstrap $ (,)
         <$> areq textField "Nome: " Nothing
         <*> areq emailField "E-mail: " Nothing
     <*> areq (selectField $ optionsPairs [(MsgValue1, "value1"),(MsgValue2, "value2")]) "Which value?" Nothing
-    
 
 getHomeR :: Handler Html
 getHomeR = do
     (widget,_) <- generateFormPost formSig
     msg <- getMessage
     defaultLayout $ do
-       -- toWidgetHead([hamlet|<meta name=viewport content=#{width=device width, initial scale=1.0}>|])
+    -- toWidgetHead([hamlet|<meta name=viewport content=#{width=device width, initial scale=1.0}>|])
         addScriptRemote "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"
         toWidgetHead $(luciusFile "templates/css.lucius")
         toWidgetHead $(luciusFile "templates/mobile.lucius")
