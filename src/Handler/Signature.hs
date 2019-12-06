26ad43e82ecd6b5a07ce714ba50bd08e524c82fa
Will <demaroto69@gmail.com>
2019-12-06 20:55:21 -0300

    deploy

---
+++ src/Handler/Signature.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+module Handler.Signature where
+
+import Import
+import Text.Lucius
+import Text.Julius
+-- import Network.HTTP.Types.Status
+import Database.Persist.Postgresql
+
+
+getListSignatureR :: Handler Html
+getListSignatureR = do
+    -- select * from signature order by signature.nome
+    signatures <- runDB $ selectList [] [Asc SignatureNome]
+    defaultLayout $ do 
+        $(whamletFile "templates/denuncia.hamlet")
