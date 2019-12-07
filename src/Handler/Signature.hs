1429be100058bcd24fbfcd2c081afcf893bdf028
Will <demaroto69@gmail.com>
2019-12-07 13:24:08 -0300

    Arrumando algumas coisas

--- src/Handler/Signature.hs
+++ src/Handler/Signature.hs
@@ -18,4 +18,12 @@ getListSignatureR = do
     -- select * from signature order by signature.nome
     signatures <- runDB $ selectList [] [Asc SignatureName]
     defaultLayout $ do 
+        toWidgetHead [hamlet|
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        |]
+        setTitle "Petição Verão | Assine"
+        addScriptRemote "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"
+        toWidgetHead $(luciusFile "templates/css.lucius")
+        toWidgetHead $(luciusFile "templates/mobile.lucius")
+        toWidgetHead $(juliusFile "templates/js.julius")
         $(whamletFile "templates/assinaturas.hamlet")
